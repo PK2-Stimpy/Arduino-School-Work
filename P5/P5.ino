76688c38c4f14461db9b5dc3b3f2f4f8359b10f5
PK2_Stimpy <31771533+fernandoGamer12@users.noreply.github.com>
2021-02-04 12:26:08 +0100

    Create P5.ino

---
+++ P5/P5.ino
@@ -0,0 +1,21 @@
+const int pins[] = {2, 3, 4, 5, 6, 7, 8};
+int waitTime = 50;
+
+void setup() {
+  for(int i = 0; i < sizeof(pins); i++)
+    pinMode(pins[i], OUTPUT);
+}
+
+void loop() {
+  for(int i = 0; i < sizeof(pins); i++) {
+    digitalWrite(pins[i], HIGH);
+    delay(waitTime);
+    digitalWrite(pins[i], LOW);
+  }
+  delay(waitTime*2);
+  for(int i = 0; i < sizeof(pins); i--) {
+    digitalWrite(pins[i], HIGH);
+    delay(waitTime);
+    digitalWrite(pins[i], LOW);
+  }
+}
